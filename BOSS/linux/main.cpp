bec2efcdf6d9c6b8a1bd304e0189104c7c018e3e
Steve French <sfrench@blueoceanseismic.com>
2020-10-26 17:06:02 +0800

    Logging mainly

--- BOSS/linux/main.cpp
+++ BOSS/linux/main.cpp
@@ -35,16 +35,20 @@ public:
 
         return serial_raii(fh);
     }
-    ~serial_raii() { close(_fh); }
+    ~serial_raii()
+    {
+        spdlog::debug("Closing serial file handle: {}", _fh);
+        close(_fh);
+    }
     int get_file_handle() { return _fh; }
 };
 
-const uint8_t ID = 45;
+uint8_t id = 45;
 
 const uint32_t BAUD = 115200;
-const std::string SERIAL_FILE = "/dev/ttySO1"; // todo:
+const std::string SERIAL_FILE = "/dev/ttyO1"; // todo:
 
-PJONThroughSerial bus(ID);
+PJONThroughSerial bus;
 
 void receiver_function(uint8_t *payload,
                        uint16_t length,
@@ -61,7 +65,7 @@ void receiver_function(uint8_t *payload,
     ss << " packet info: ";
     ss << packet_info.header;
 
-    std::cout << ss.str() << std::endl;
+    spdlog::info(ss.str());
 };
 
 int main(int argc, char *argv[])
@@ -69,36 +73,78 @@ int main(int argc, char *argv[])
     spdlog::info("Command line arguments:");
     for (auto i = 0; i < argc; i++)
     {
-        spdlog::info(argv[i]);
+        spdlog::info("\t{}", argv[i]);
     }
 
-    po::options_description desc("Allowed command line arguments");
-    desc.add_options()("PJON ID", po::value<uint8_t>(), "Bus ID to use for PJON");
+    po::options_description desc("Command line arguments");
+    desc.add_options()("id", po::value<uint8_t>()->required(), "Bus ID to use for PJON")("ll", po::value<std::string>(), "Log levels: trace, debug, info, warning, error, critical, off");
 
     po::variables_map vm;
-    po::store(po::parse_command_line(argc, argv, desc), vm);
-    po::notify(vm);
+    try
+    {
+        po::store(po::parse_command_line(argc, argv, desc), vm);
+        po::notify(vm);
+        spdlog::debug("notify");
+    }
+    catch (std::exception &e)
+    {
+        spdlog::error(e.what());
+        return -1;
+    }
 
-    spdlog::info("Setting reciever");
+    id = vm["id"].as<uint8_t>();
 
-    bus.set_receiver(receiver_function);
+    bus.set_bus_id(&id);
 
-    spdlog::info("Opening serial... \n");
+    std::string ll = vm["ll"].empty() ? "trace"s : vm["ll"].as<std::string>();
+
+    spdlog::info("Log level: {}", ll);
+
+    try
+    {
+        spdlog::set_level(spdlog::level::from_str(ll));
+    }
+    catch (std::exception &e)
+    {
+        spdlog::error("Set incorrect log level: '{}', defaulting to 'trace'", ll);
+    }
+
+    spdlog::trace("Setting reciever function");
+
+    bus.set_receiver(
+        [](uint8_t *payload,
+           uint16_t length,
+           const PJON_Packet_Info &packet_info) {
+            /* Make use of the payload before sending something, the buffer where payload points to is
+            overwritten when a new message is dispatched */
+            std::stringstream ss;
+            ss << "payload :" << std::hex;
+            for (auto i = 0; i < length; i++)
+            {
+                ss << payload[i];
+            }
+            ss << " packet info: ";
+            ss << packet_info.header << packet_info.hops; //todo: print all data
+
+            spdlog::info(ss.str());
+        });
+
+    spdlog::info("Opening serial");
     try
     {
         serial_raii serial = serial_raii::init(SERIAL_FILE.c_str(), BAUD);
 
-        spdlog::info("Setting serial");
+        spdlog::trace("Setting serial");
         bus.strategy.set_serial((uint16_t)serial.get_file_handle());
         bus.strategy.set_baud_rate(BAUD);
 
-        spdlog::info("Opening bus... \n");
+        spdlog::info("Opening bus");
         bus.begin();
-        spdlog::info("Attempting to roll bus... \n");
+        spdlog::info("Attempting to roll bus");
         bus.update();
-        spdlog::info("Attempting to receive from bus... \n");
+        spdlog::info("Attempting to receive from bus");
         bus.receive();
-        spdlog::info("Success! \n");
+        spdlog::info("Success");
 
         while (true)
         {
